abd29258bed27523abfb65e8b1d7f72b7e339907
Gustavo Castellanos <gustavoaca1997@gmail.com>
2018-06-07 23:55:26 -0400

    Se cambia nombre a BasicTran

--- compile.sh
+++ compile.sh
@@ -3,9 +3,9 @@ happy Parser.y
 
 case $(whoami) in
     german)
-        stack ghc -- Lex.hs Parser.hs Main.hs -o basictrans
+        stack ghc -- Lex.hs Parser.hs Main.hs -o BasicTran
         ;;
     *)
-        ghc Main.hs -o basictrans
+        ghc Main.hs -o BasicTran
         ;;
 esac
